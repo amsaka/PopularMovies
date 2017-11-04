57e5b5e4ee6c8d5ea4d8c490d5036f03ded1dd80
Ameya K <japsk24@gmail.com>
2017-11-05 01:15:58 +0530

    Chore: Packages creation
    Packages created for UI and API classes

--- PopularMovies/app/src/main/java/movies/skills/swami/com/popularmovies/screen/DiscoveryActivity.java
+++ PopularMovies/app/src/main/java/movies/skills/swami/com/popularmovies/screen/DiscoveryActivity.java
@@ -1,7 +1,9 @@
-package movies.skills.swami.com.popularmovies;
+package movies.skills.swami.com.popularmovies.screen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import movies.skills.swami.com.popularmovies.R;
 
 public class DiscoveryActivity extends AppCompatActivity {
 
